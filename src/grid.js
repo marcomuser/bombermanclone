6a080f4166bd563628686b7d0e05a0e431eb74de
Marco Muser <marco.muser@posteo.de>
2020-06-17 12:05:14 +0200

    add helper functions to check whether columns or rows contain obstacles

--- src/grid.js
+++ src/grid.js
@@ -2,7 +2,7 @@ const grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0],
+    [0, 5, 0, 5, 5, 5, 0, 5, 0, 5, 0, 5, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,6 +14,19 @@ const grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
+function checkRowForObstacle(row) {
+    for (let i = 0; i < grid[row].length; i++) {
+        if (grid[row][i] === 5) return true
+    }
+    return false;
+}
+
+function checkColForObstacle(col) {
+    for (let i = 0; i < grid[col].length; i++) {
+        if (grid[i][col] === 5) return true
+    }
+    return false;
+}
 
 /*
 0: squareField is empty
