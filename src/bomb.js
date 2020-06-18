cec765d30e0f86937c57ac3d9a5b7e625a1e5719
Marco Muser <marco.muser@posteo.de>
2020-06-18 17:08:34 +0200

    add functionality for visualizing bomb explosions

--- src/bomb.js
+++ src/bomb.js
@@ -18,5 +18,32 @@ class Bomb {
         if (!this.explodes) {
             image(bombImage, this.col, this.row, squareSize, squareSize);
         }
+        if (this.explodes) {
+            if (checkColForObstacle(this.x) && !checkRowForObstacle(this.y)) {
+                // console.log('bomb destroyed a whole row');
+                for (let i = 0; i < 13; i++) {
+                    if (this.y === i) {
+                        line(0, this.row + squareSize/2, canvasWidth, this.row + squareSize/2);
+                    }
+                }
+            } else if (!checkColForObstacle(this.x) && checkRowForObstacle(this.y)) {
+                // console.log('bomb destroyed a whole column');
+                for (let i = 0; i < 13; i++) {
+                    if (this.x === i) {
+                        line(this.col + squareSize/2, 0, this.col + squareSize/2, canvasHeight);
+                    }
+                }
+            } else {
+                // console.log('bomb destroyed both row and column');
+                for (let i = 0; i < 13; i++) {
+                    if (this.x === i) {
+                        line(this.col + squareSize/2, 0, this.col + squareSize/2, canvasHeight);
+                    }
+                    if (this.y === i) {
+                        line(0, this.row + squareSize/2, canvasWidth, this.row + squareSize/2);
+                    }
+                }
+            }
+        }
     }
 }
